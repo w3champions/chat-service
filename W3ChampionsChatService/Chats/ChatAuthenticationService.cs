38a21278736ea610270de50ed28d5edfa90c2b29
Simon Heiss <simonheiss87@gmail.com>
2020-11-28 10:59:18 +0100

    chate details loading added

---
+++ W3ChampionsChatService/Chats/ChatAuthenticationService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+
+namespace W3ChampionsChatService.Chats
+{
+    public class ChatAuthenticationService : MongoDbRepositoryBase
+    {
+        private static readonly string StatisticServiceApiUrl = Environment.GetEnvironmentVariable("STATISTIC_SERVICE_URI") ?? "https://statistic-service.test.w3champions.com";
+        public ChatAuthenticationService(MongoClient mongoClient) : base(mongoClient)
+        {
+        }
+
+        public async Task<ChatUser> GetUser(string battleTag)
+        {
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(StatisticServiceApiUrl);
+            var result = await httpClient.GetAsync($"/api/player/{battleTag}/clan-and-picture");
+            var content = await result.Content.ReadAsStringAsync();
+            var userDetails = JsonConvert.DeserializeObject<ChatDetailsDto>(content);
+            return new ChatUser(battleTag, userDetails?.ClanId, userDetails?.ProfilePicture);
+        }
+    }
+}
--- W3ChampionsChatService/Chats/ChatUser.cs
+++ W3ChampionsChatService/Chats/ChatUser.cs
@@ -4,14 +4,42 @@ namespace W3ChampionsChatService.Chats
 {
     public class ChatUser
     {
-        public ChatUser(string battleTag)
+        public ChatUser(string battleTag, string clanTag, ProfilePicture profilePicture)
         {
             BattleTag = battleTag;
+            ClanTag = clanTag;
+            ProfilePicture = profilePicture;
             Name = battleTag.Split("#")[0];
         }
 
         [BsonId]
         public string BattleTag { get; set; }
         public string Name { get; set; }
+        public string ClanTag { get; set; }
+        public ProfilePicture ProfilePicture { get; set; }
+    }
+
+    public class ProfilePicture
+    {
+        public AvatarCategory Race { get; set; }
+        public long PictureId { get; set; }
+        public bool IsClassic { get; set; }
+    }
+
+    public enum AvatarCategory
+    {
+        RnD = 0,
+        HU = 1,
+        OC = 2,
+        NE = 4,
+        UD = 8,
+        Total = 16,
+        Special = 32
+    }
+
+    public class ChatDetailsDto
+    {
+        public string ClanId { get; set; }
+        public ProfilePicture ProfilePicture { get; set;}
     }
 }
