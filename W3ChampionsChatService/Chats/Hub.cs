d6366056578c7f55693e2a93545b6b70ba2d5f8f
Simon Heiss <simonheiss87@gmail.com>
2020-11-28 22:00:42 +0100

    try real logger again

--- W3ChampionsChatService/Chats/Hub.cs
+++ W3ChampionsChatService/Chats/Hub.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
@@ -81,36 +82,48 @@ namespace W3ChampionsChatService.Chats
 
         public async Task LoginAs(string chatKey, string battleTag)
         {
-            _logger.LogInformation("login started");
-            var user = await _authenticationService.GetUser(battleTag);
-            _logger.LogInformation("BT" + user.BattleTag);
-            _logger.LogInformation("CT" + user.ClanTag);
-            var memberShip = await _settingsRepository.Load(battleTag) ?? new ChatSettings(battleTag);
-            _logger.LogInformation("MBT" + memberShip.BattleTag);
-            _logger.LogInformation("DC" + memberShip.DefaultChat);
-
-            var ban = await _banRepository.Load(battleTag.ToLower());
-            _logger.LogInformation("BT" + ban?.BattleTag);
-
-
-            var nowDate = DateTime.Now.ToString("yyyy-MM-dd");
-            if (ban != null && string.Compare(ban.EndDate, nowDate, StringComparison.Ordinal) > 0)
+            try
             {
-                await Clients.Caller.SendAsync("PlayerBannedFromChat", ban);
+                _logger.LogInformation("login started");
+                var user = await _authenticationService.GetUser(battleTag);
+                _logger.LogInformation("BT" + user.BattleTag);
+                _logger.LogInformation("CT" + user.ClanTag);
+                var memberShip = await _settingsRepository.Load(battleTag) ?? new ChatSettings(battleTag);
+                _logger.LogInformation("MBT" + memberShip.BattleTag);
+                _logger.LogInformation("DC" + memberShip.DefaultChat);
+
+                var ban = await _banRepository.Load(battleTag.ToLower());
+                _logger.LogInformation("BT" + ban?.BattleTag);
+
+
+                var nowDate = DateTime.Now.ToString("yyyy-MM-dd");
+                if (ban != null && string.Compare(ban.EndDate, nowDate, StringComparison.Ordinal) > 0)
+                {
+                    await Clients.Caller.SendAsync("PlayerBannedFromChat", ban);
+                }
+                else
+                {
+                    _logger.LogInformation("1");
+                    _connections.Add(Context.ConnectionId, memberShip.DefaultChat, user);
+                    _logger.LogInformation("2");
+                    await Groups.AddToGroupAsync(Context.ConnectionId, memberShip.DefaultChat);
+                    _logger.LogInformation("3");
+                    var usersOfRoom = _connections.GetUsersOfRoom(memberShip.DefaultChat);
+                    _logger.LogInformation("4");
+                    await Clients.Group(memberShip.DefaultChat).SendAsync("UserEntered", user);
+                    _logger.LogInformation("5");
+                    await Clients.Caller.SendAsync("StartChat", usersOfRoom, _chatHistory.GetMessages(memberShip.DefaultChat), memberShip.DefaultChat);
+                }
             }
-            else
+            catch (Exception e)
             {
-                _logger.LogInformation("1");
-                _connections.Add(Context.ConnectionId, memberShip.DefaultChat, user);
-                _logger.LogInformation("2");
-                await Groups.AddToGroupAsync(Context.ConnectionId, memberShip.DefaultChat);
-                _logger.LogInformation("3");
-                var usersOfRoom = _connections.GetUsersOfRoom(memberShip.DefaultChat);
-                _logger.LogInformation("4");
-                await Clients.Group(memberShip.DefaultChat).SendAsync("UserEntered", user);
-                _logger.LogInformation("5");
-                await Clients.Caller.SendAsync("StartChat", usersOfRoom, _chatHistory.GetMessages(memberShip.DefaultChat), memberShip.DefaultChat);
+
+                await Clients.Caller.SendAsync("StartChat", new List<ChatUser>(), new List<ChatMessage>
+                {
+                    new ChatMessage(new ChatUser("error#123", "clanerror", new ProfilePicture()), e.Message)
+                }, "error");
             }
+
         }
     }
 }
