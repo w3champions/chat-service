eeba2de0a147b3bca71ae894c9966b0a9c57c23a
Simon Heiss <simonheiss87@gmail.com>
2020-11-28 22:19:08 +0100

    now with aspnet...

--- W3ChampionsChatService/Chats/Hub.cs
+++ W3ChampionsChatService/Chats/Hub.cs
@@ -11,7 +11,6 @@ namespace W3ChampionsChatService.Chats
 {
     public class ChatHub : Hub
     {
-        private readonly ILogger<ChatHub> _logger;
         private readonly ChatAuthenticationService _authenticationService;
         private readonly BanRepository _banRepository;
         private readonly SettingsRepository _settingsRepository;
@@ -22,10 +21,8 @@ namespace W3ChampionsChatService.Chats
             BanRepository banRepository,
             SettingsRepository settingsRepository,
             ConnectionMapping connections,
-            ChatHistory chatHistory,
-            ILogger<ChatHub> logger = null)
+            ChatHistory chatHistory)
         {
-            _logger = logger ?? new NullLogger<ChatHub>();
             _authenticationService = authenticationService;
             _banRepository = banRepository;
             _settingsRepository = settingsRepository;
@@ -82,48 +79,24 @@ namespace W3ChampionsChatService.Chats
 
         public async Task LoginAs(string chatKey, string battleTag)
         {
-            try
+            var user = await _authenticationService.GetUser(battleTag);
+            var memberShip = await _settingsRepository.Load(battleTag) ?? new ChatSettings(battleTag);
+
+            var ban = await _banRepository.Load(battleTag.ToLower());
+
+            var nowDate = DateTime.Now.ToString("yyyy-MM-dd");
+            if (ban != null && string.Compare(ban.EndDate, nowDate, StringComparison.Ordinal) > 0)
             {
-                _logger.LogInformation("login started");
-                var user = await _authenticationService.GetUser(battleTag);
-                _logger.LogInformation("BT" + user.BattleTag);
-                _logger.LogInformation("CT" + user.ClanTag);
-                var memberShip = await _settingsRepository.Load(battleTag) ?? new ChatSettings(battleTag);
-                _logger.LogInformation("MBT" + memberShip.BattleTag);
-                _logger.LogInformation("DC" + memberShip.DefaultChat);
-
-                var ban = await _banRepository.Load(battleTag.ToLower());
-                _logger.LogInformation("BT" + ban?.BattleTag);
-
-
-                var nowDate = DateTime.Now.ToString("yyyy-MM-dd");
-                if (ban != null && string.Compare(ban.EndDate, nowDate, StringComparison.Ordinal) > 0)
-                {
-                    await Clients.Caller.SendAsync("PlayerBannedFromChat", ban);
-                }
-                else
-                {
-                    _logger.LogInformation("1");
-                    _connections.Add(Context.ConnectionId, memberShip.DefaultChat, user);
-                    _logger.LogInformation("2");
-                    await Groups.AddToGroupAsync(Context.ConnectionId, memberShip.DefaultChat);
-                    _logger.LogInformation("3");
-                    var usersOfRoom = _connections.GetUsersOfRoom(memberShip.DefaultChat);
-                    _logger.LogInformation("4");
-                    await Clients.Group(memberShip.DefaultChat).SendAsync("UserEntered", user);
-                    _logger.LogInformation("5");
-                    await Clients.Caller.SendAsync("StartChat", usersOfRoom, _chatHistory.GetMessages(memberShip.DefaultChat), memberShip.DefaultChat);
-                }
+                await Clients.Caller.SendAsync("PlayerBannedFromChat", ban);
             }
-            catch (Exception e)
+            else
             {
-
-                await Clients.Caller.SendAsync("StartChat", new List<ChatUser>(), new List<ChatMessage>
-                {
-                    new ChatMessage(new ChatUser("error#123", "clanerror", new ProfilePicture()), $"{e.Message} STACK: {e.StackTrace}")
-                }, "error");
+                _connections.Add(Context.ConnectionId, memberShip.DefaultChat, user);
+                await Groups.AddToGroupAsync(Context.ConnectionId, memberShip.DefaultChat);
+                var usersOfRoom = _connections.GetUsersOfRoom(memberShip.DefaultChat);
+                await Clients.Group(memberShip.DefaultChat).SendAsync("UserEntered", user);
+                await Clients.Caller.SendAsync("StartChat", usersOfRoom, _chatHistory.GetMessages(memberShip.DefaultChat), memberShip.DefaultChat);
             }
-
         }
     }
 }
