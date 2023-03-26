d6774615849f945968bf414a00c30f1b3648b0f0
Gustav Andersson <gustav87and@gmail.com>
2023-03-26 16:24:52 +0200

    Add admin secret checking

--- W3ChampionsChatService/Mutes/MuteController.cs
+++ W3ChampionsChatService/Mutes/MuteController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using W3ChampionsChatService.Authentication;
@@ -8,6 +9,7 @@ namespace W3ChampionsChatService.Mutes
     [Route("api/loungeMute")]
     public class MuteController : ControllerBase
     {
+        private static readonly string AdminSecret = Environment.GetEnvironmentVariable("ADMIN_SECRET") ?? "300C018C-6321-4BAB-B289-9CB3DB760CBB";
         private readonly MuteRepository _muteRepository;
         public MuteController(MuteRepository muteRepository){
             _muteRepository = muteRepository;
@@ -15,20 +17,25 @@ namespace W3ChampionsChatService.Mutes
 
         [HttpGet("")]
         [CheckIfBattleTagIsAdmin]
-        public async Task<IActionResult> GetLoungeMutes()
+        public async Task<IActionResult> GetLoungeMutes(string secret)
         {
+            if (secret != AdminSecret) {
+                return StatusCode(403);
+            }
             var loungeMutes = await _muteRepository.GetLoungeMutes();
             return Ok(loungeMutes);
         }
 
         [HttpPost("")]
         [CheckIfBattleTagIsAdmin]
-        public async Task<IActionResult> AddLoungeMute([FromBody] LoungeMuteRequest loungeMuteRequest)
+        public async Task<IActionResult> AddLoungeMute([FromBody] LoungeMuteRequest loungeMuteRequest, string secret)
         {
+            if (secret != AdminSecret) {
+                return StatusCode(403);
+            }
             if (loungeMuteRequest.battleTag == "") {
                 return BadRequest("BattleTag cannot be empty.");
             }
-
             if (loungeMuteRequest.endDate == "") {
                 return BadRequest("Ban End Date must be set.");
             }
@@ -39,10 +46,10 @@ namespace W3ChampionsChatService.Mutes
 
         [HttpDelete("{bTag}")]
         [CheckIfBattleTagIsAdmin]
-        public async Task<IActionResult> DeleteLoungeMute([FromRoute] string bTag)
+        public async Task<IActionResult> DeleteLoungeMute([FromRoute] string bTag, string secret)
         {
-            if (bTag == "") {
-                return BadRequest("BattleTag not specified.");
+            if (secret != AdminSecret) {
+                return StatusCode(403);
             }
             await _muteRepository.DeleteLoungeMute(bTag);
             return Ok();
