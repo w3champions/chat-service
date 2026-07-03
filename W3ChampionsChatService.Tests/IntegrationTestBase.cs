5d6e158d367004c3577e092bcadffd9940738dad
Marco de Abreu <18629099+marcoabreu@users.noreply.github.com>
2026-07-03 06:17:20 +0200

    test: run integration tests against ephemeral local Mongo via Testcontainers

--- W3ChampionsChatService.Tests/IntegrationTestBase.cs
+++ W3ChampionsChatService.Tests/IntegrationTestBase.cs
@@ -6,8 +6,7 @@ namespace W3ChampionsChatService.Tests;
 
 public class IntegrationTestBase
 {
-    //protected readonly MongoClient MongoClient = new MongoClient("mongodb://localhost:27017/");
-    protected readonly MongoClient MongoClient = new MongoClient("mongodb://157.90.1.251:3512/");
+    protected MongoClient MongoClient => MongoTestServer.Client;
 
     [SetUp]
     public async Task Setup()
